7a1dc60a76b7f7dd0b646fcd1624b4b876a3c17e
Gustavo Tessaro <gu.tessaro@hotmail.com>
2026-06-04 00:16:00 -0300

    Arrumando campo nome maximo 50 caracteres categoria

--- ListaDeCompras.WebApplication/ModuloCategoria/Apresentacao/CategoriaViewModels.cs
+++ ListaDeCompras.WebApplication/ModuloCategoria/Apresentacao/CategoriaViewModels.cs
@@ -10,7 +10,7 @@ public record ListarCategoriasViewModel(
 
 public record CadastrarCategoriaViewModel(
     [Required(ErrorMessage = "O campo \"Nome\" deve ser preenchido.")]
-[StringLength(100, ErrorMessage = "O campo \"Nome\" deve conter no máximo 100 caracteres.")]
+[StringLength(50, ErrorMessage = "O campo \"Nome\" deve conter no máximo 50 caracteres.")]
 string Nome,
 
     [Required(ErrorMessage = "O campo \"Cor\" deve ser preenchido.")]
